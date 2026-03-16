7ad50b4afe0e85971d47d4aa7c7a92070f29bbc2
M Alhassoun <mo@boxlink.net>
2026-03-16 17:01:30 +0300

    Add incoming shipment admin notification hook

--- kwtsms_sms/models/stock_picking.py
+++ kwtsms_sms/models/stock_picking.py
@@ -10,7 +10,7 @@ _logger = logging.getLogger(__name__)
 class StockPicking(models.Model):
     """Hook into stock.picking._action_done to send SMS notifications."""
 
-    _inherit = ['stock.picking', 'kwtsms.notification.mixin']
+    _inherit = ['stock.picking', 'kwtsms.notification.mixin', 'kwtsms.admin.notification.mixin']
     _name = 'stock.picking'
     _description = 'Transfer'
 
@@ -30,6 +30,27 @@ class StockPicking(models.Model):
             except Exception as e:
                 _logger.error(
                     'kwtSMS: Failed to send delivery SMS for %s: %s',
+                    picking.name, e,
+                )
+
+        for picking in self:
+            if picking.picking_type_code != 'incoming':
+                continue
+            try:
+                picking._kwtsms_send_admin_notification(
+                    'admin_incoming_shipment',
+                    'kwtsms.auto_admin_incoming_shipment',
+                    'kwtsms.admin_phone_inventory',
+                    {
+                        'company_name': picking.company_id.name or '',
+                        'picking_name': picking.name or '',
+                        'supplier_name': picking.partner_id.name or '',
+                        'product_count': str(len(picking.move_ids)),
+                    },
+                )
+            except Exception as e:
+                _logger.error(
+                    'kwtSMS: Admin incoming shipment SMS failed for %s: %s',
                     picking.name, e,
                 )
 
